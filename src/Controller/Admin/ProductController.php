bc817b6387f314a136485376632e10b73ecfcede
Joyce Carvalho <goncalvesjoyce8@gmail.com>
2021-09-15 21:36:08 -0300

    Configurando as páginas de CRUD dos Produtos

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,24 +22,34 @@ class ProductController extends AbstractController
     #[Route('/create', name: 'create_products')]
     public function create() 
     {
-        
+        return $this->render('admin/product/create.html.twig');
     }
 
     #[Route('/store', name: 'store_products')]
-    public function store() 
+    public function store(Request $request) 
     {
-        $product = new Product();   
-        $product->setName('Produto Teste');
-        $product->setDescription('Descrição');
-        $product->setBody('Info Produto');
-        $product->setPrice(3990);
-        $product->setSlug('product-test');
-        $product->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
-        $product->setUpdateAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
+        try {
+            
+            $data = $request->request->all();
+        
+            $product = new Product();   
+            $product->setName($data["name"]);
+            $product->setDescription($data["description"]);
+            $product->setBody($data["body"]);
+            $product->setPrice($data["price"]);
+            $product->setSlug($data["slug"]);
+            $product->setCreatedAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
+            $product->setUpdateAt(new \DateTime('now', new \DateTimeZone('America/Sao_Paulo')));
 
-        $manager = $this->getDoctrine()->getManager();
-        $manager->persist($product);
-        $manager->flush();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($product);
+            $manager->flush();
+
+            return $this->redirectToRoute('admin_index_products');
+
+        } catch (\Exception $th) {
+            die($th->getMessage());
+        }
     }
 
     #[Route('/edit/{product}', name: 'edit_products')]
